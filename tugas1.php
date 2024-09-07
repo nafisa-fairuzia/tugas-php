c7bc8ed38a5540b4973786d0b2aa776628da8cb9
nafisa-fairuzia <nafisafairuzia@gmail.com>
2024-09-07 16:45:09 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,22 @@
+<?php
+$bulan = 3;
+switch ($bulan) {
+    case 1:
+        echo "Isi variabel bulan adalah January";
+        break;
+    case 2:
+        echo "Isi variabel bulan adalah February";
+        break;
+    case 3:
+        echo "Isi variabel bulan adalah Maret";
+        break;
+    case 4:
+        echo "Isi variabel bulan adalah April";
+        break;
+    case 5:
+        echo "Isi variabel bulan adalah Mei";
+        break;
+    default:
+        echo "Isi variabel bulan Tidak ditemukan";
+}
+?>
